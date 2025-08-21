dc38ff726b5c47a31703ef3de7aea4a109640ead
anthkta <anramosmd1@protonmail.com>
2025-08-20 21:42:47 -0500

    clase de loops

---
+++ src/review_codicionales.java
@@ -0,0 +1,10 @@
+public class review_codicionales {
+    public static void main(String[] args) {
+        int edad = 33;
+        if (edad >= 21) {
+            System.out.println("Puede tomar");
+        } else {
+            System.out.println("No puede tomar");
+        }
+    }
+}
