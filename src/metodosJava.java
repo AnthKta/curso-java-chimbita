eeaf2374639811b8029eabca1e774a18c4b693af
Anthkta <anramosmd1@protonmail.com>
2025-08-29 21:51:33 -0500

    Metodos java

---
+++ src/metodosJava.java
@@ -0,0 +1,9 @@
+public class metodosJava {
+    public static void saludo() { //declarando
+      System.out.println("Que mas puesh OMEEE!!!");
+    }
+
+  public static void main(String[] args) {
+    saludo(); //llamando
+  }
+}
