7a56858e05ad38ad52d801ec0379165e95aba4fe
Anthkta <anramosmd1@protonmail.com>
2025-09-04 23:01:33 -0500

    Metodos 2

---
+++ src/metodoParametrosSuma.java
@@ -0,0 +1,8 @@
+public class metodoParametrosSuma {
+  public static void suma(int a, int b) {
+    System.out.println("La suma es: " + (a + b));
+  }
+  public static void main(String[] args) {
+    suma(3, 6);
+  }
+}
